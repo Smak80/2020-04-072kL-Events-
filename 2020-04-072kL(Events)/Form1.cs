54cf4686ba2c3b4b1edd71d2613dd8190f2484bd
Smak80 <smak-80@yandex.ru>
2020-04-07 09:59:36 +0300

    Полная лекционная версия.

--- 2020-04-072kL(Events)/Form1.cs
+++ 2020-04-072kL(Events)/Form1.cs
@@ -16,5 +16,46 @@ namespace _2020_04_072kL_Events_
         {
             InitializeComponent();
         }
+
+        private void OnProgress(int value)
+        {
+            if (!progressBar1.InvokeRequired)
+                progressBar1.Value = value;
+            else
+                Invoke(new Processor.Progress(OnProgress), value);
+        }
+
+        private void OnProgressValue(int value)
+        {
+            if (!label1.InvokeRequired)
+                label1.Text = "Выполнено: "+value.ToString()+"%";
+            else
+                Invoke(new Processor.Progress(OnProgressValue), value);
+        }
+
+        private void OnFinish(bool res, int resVal)
+        {
+            if (!label1.InvokeRequired)
+            {
+                if (res)
+                    label1.Text = "Результат вычислений: " + resVal;
+                else
+                    label1.Text = "Результат не был получен :(";
+            }
+            else
+            {
+                object[] pars = { res, resVal };
+                Invoke(new Processor.Finish(OnFinish),  pars);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Processor p = new Processor();
+            p.EventProgress += OnProgress;
+            p.EventProgress += OnProgressValue;
+            p.EventFinish += OnFinish;
+            p.Start();
+        }
     }
 }
---
+++ 2020-04-072kL(Events)/Processor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _2020_04_072kL_Events_
+{
+    class Processor
+    {
+        private int donePercent = 0;
+
+        public delegate void Progress(int value);
+
+        public delegate void Finish(bool result, int resultValue);
+
+        public event Progress EventProgress;
+        public event Finish EventFinish;
+        
+        private void Work()
+        {
+            donePercent = 0;
+            while (donePercent < 100)
+            {
+                Thread.Sleep(1000);
+                donePercent += 10;
+                EventProgress?.Invoke(donePercent);
+            }
+            Random r = new Random();
+            bool b = r.Next(100) % 2 == 0;
+            int res = r.Next(2000, 3000);
+            EventFinish?.Invoke(b, res);
+        }
+
+        public void Start()
+        {
+            new Thread(new ThreadStart(Work)).Start();
+        }
+    }
+}
