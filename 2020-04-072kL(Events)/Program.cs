0fdf32d07872199164d25c9a3fba3b5541cdd988
Smak80 <smak-80@yandex.ru>
2020-04-07 08:28:10 +0300

    Добавьте файлы проекта.

---
+++ 2020-04-072kL(Events)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2020_04_072kL_Events_
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
